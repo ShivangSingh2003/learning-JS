8ab87c0c97532390912598917e7cda534c003adb
ShivangSingh2003 <94367469+ShivangSingh2003@users.noreply.github.com>
2024-08-16 21:35:54 +0000

    DataType Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "43abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33" -> 33
+//"33abc" -> NaN
+
+let isLoggedIn = "5"
+let boolIsLoggedin = Boolean(isLoggedIn)
+console.log(boolIsLoggedin)
+
+let someNumber = 66
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
