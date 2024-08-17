7367bfd195ccebac80101934022384a2f67f38be
ShivangSingh2003 <94367469+ShivangSingh2003@users.noreply.github.com>
2024-08-17 12:51:36 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);    //comparisons and equality checks work differently
+console.log(null == 0);
+console.log(null >= 0);
+
+// === strict check -> checks datatype.....conversion does not take place before comparison like in ==
