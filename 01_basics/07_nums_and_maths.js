4dfa5c931292e9fda4fee297493e5d08440a8291
ShivangSingh2003 <94367469+ShivangSingh2003@users.noreply.github.com>
2024-08-17 19:09:04 +0000

    maths and numbers

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,45 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const num  = 123.5856
+// console.log(num.toPrecision(2));
+
+const zeros = 100000000
+// console.log(zeros.toLocaleString('en-IN'));
+
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_SAFE_INTEGER);
+// console.log(Number.MAX_SAFE_INTEGER);
+
+//++++++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.7));
+// console.log(Math.min(2,5,1,7,8));
+// console.log(Math.max(2,5,1,7,8));
+// console.log(Math.pow(2,3));
+
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor((Math.random()*10)) + 1);
+
+// To obtain a random no. within given range
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random() * (max - min + 1)) + min));
+
+
