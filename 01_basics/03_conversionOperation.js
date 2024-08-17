4fbcb07c7b5df0fc1a2b11dda59c96b9d13689c3
ShivangSingh2003 <94367469+ShivangSingh2003@users.noreply.github.com>
2024-08-17 12:51:06 +0000

    conversions and operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,20 +1,51 @@
 let score = "43abc"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 //"33" -> 33
 //"33abc" -> NaN
 
 let isLoggedIn = "5"
 let boolIsLoggedin = Boolean(isLoggedIn)
-console.log(boolIsLoggedin)
+//console.log(boolIsLoggedin)
 
 let someNumber = 66
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+//console.log(stringNumber)
+//console.log(typeof stringNumber);
+
+//*********************************************************OPERATIONS***********************************************************
+
+let value = 3
+let negValue = -value 
+//console.log(negValue)
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**5)
+//console.log(2/3)
+//console.log(2%3)
+
+let str1 = "hello"
+let str2 = " Shivang"
+
+let str3 = str1+str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2) // string
+console.log(1 + 2 + "2") // addition then string concat
+
+let counter = 100
+console.log(counter)
+++counter
+console.log(counter)
+counter++
+console.log(counter)
