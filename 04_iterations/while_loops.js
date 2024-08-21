2db1fc8264c2b3785bfa85ae63ec1ec55fe3c455
ShivangSingh2003 <94367469+ShivangSingh2003@users.noreply.github.com>
2024-08-21 16:46:31 +0000

    while and do while loops

---
+++ 04_iterations/while_loops.js
@@ -0,0 +1,15 @@
+//while loops
+
+let index = 0
+
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index += 2
+}
+
+let score = 11
+
+do {
+    console.log(`The score is ${score}`);
+    score++
+} while (score<=10);
