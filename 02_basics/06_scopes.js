dc63498fe59ed6b77176d28a8ade2145bbeae102
ShivangSingh2003 <94367469+ShivangSingh2003@users.noreply.github.com>
2024-08-21 07:32:39 +0000

    scopes

--- 02_basics/06_scopes.js
+++ 02_basics/06_scopes.js
@@ -12,6 +12,54 @@ if(true) {
     var c = 10
 }
 
-console.log(a);
-console.log(b);
-console.log(c); //var doesnt follow block scope
+// console.log(a);
+// console.log(b);
+// console.log(c); //var doesnt follow block scope
+
+function one() {
+    const username = "shivang"
+    
+    function two() {
+        const website = "youtube"
+        //console.log(username);
+        
+    }
+    // console.log(website);
+    two()
+    
+}
+one()
+
+if(true) {
+    const username = "rohit"
+    if(username === "rohit") {
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+// +++++++++++++++++++++++++++++++++++++Interesting Concept++++++++++++++++++++++++++++++++++++++++++
+
+//Function
+
+console.log(addOne(5)) //Can be accessed before initialization
+function addOne(num){
+    return num+1
+}
+
+// addOne(5)
+
+//Expression
+
+console.log(addTwo(5)); //Cannot be accessed before initialization because function value stored in a variable -> Hoisting
+
+const addTwo = function(num){
+    return num + 2
+}
+
+// addTwo(5)
