eed4d2b3e358690ad632a971b40498c9c215ce64
ShivangSingh2003 <94367469+ShivangSingh2003@users.noreply.github.com>
2024-08-21 19:47:34 +0000

    for of loop

---
+++ 04_iterations/for_of_loop.js
@@ -0,0 +1,19 @@
+//for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    // console.log(num);
+    
+}
+
+const greeting = "Hello World!"
+
+for (const greet of greeting) {
+    // console.log(`Each char is ${greet}`);
+    
+}
+
